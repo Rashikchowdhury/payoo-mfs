44971daf909d02b572ae1baa98027aed50521321
Rashikchowdhury <muhammadrashikchowdhury@gmail.com>
2025-04-14 00:44:50 +0600

    created payoo log in form and basic structure of hompage

---
+++ js/login-form.js
@@ -0,0 +1,17 @@
+// console.log("file connected");
+document.getElementById('login-btn')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        const phoneNumber = document.getElementById('phone-number').value;
+
+        const pinNumber = document.getElementById('pin-number').value;
+
+        if (phoneNumber === '01921856093' && pinNumber === '1234') {
+            window.location.href = 'home.html'
+        }
+        else {
+            alert('Wrong phone number or password');
+            document.getElementById('phone-number').value = '';
+            document.getElementById('pin-number').value = '';
+        }
+    })
